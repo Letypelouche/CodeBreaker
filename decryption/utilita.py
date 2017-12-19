5a3cdd63017f257fcf6ecd82ed5815305a3094bb
Letypelouche <34472332+Letypelouche@users.noreply.github.com>
2017-12-19 10:27:34 +0100

    sauvegarde

--- decryption/utilita.py
+++ decryption/utilita.py
@@ -1,10 +1,13 @@
 from filtres import *
-
+from string import *
 def recurente(txt):
-    txt = txt.toLowerCase()
+    txt = lower(txt)
     tmp_nb = 0
     for i in range(0,25):
-        if tmp_nb < txt.count(exa_txt(i+97)):
-            rtn = exa_txt(i+97)
-            tmp_nb = txt.count(exa_txt(i+97))
+        if tmp_nb < txt.count(unichr(i+97)):
+            rtn = unichr(i+97)
+            tmp_nb = txt.count(unichr(i+97))
+        replace(txt,unichr(i+97),"")
+        if tmp_nb > count(txt):
+            break
     return rtn
--- test/test.py
+++ test/test.py
@@ -1,10 +1,11 @@
 from decryption.cesear import *
 from decryption.filtres import *
 from decryption.comparaison import *
+from decryption.utilita import *
 # print exa_txt("4242")
 # print change("a",1)
-print(fra("je suis fort !"))
-
+# print(fra("je suis fort !"))
+print(recurente("aaabbbc"))
 
 
 
